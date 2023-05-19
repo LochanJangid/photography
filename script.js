37dab736caf5d52d259903a39aaeadca2443f3e1
Lochan Jangid <129925009+LochanJangid@users.noreply.github.com>
2023-05-18 18:39:49 -0700

    🚀

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Nav
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    const scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 300) {
+      navbar.style.backgroundColor = "#000";
+    }
+    else{
+        navbar.style.backgroundColor= "transparent";
+    }
+  });
+  
+  const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', function() {
+  nav.classList.toggle('show');
+});
+
+
+// Testimonial
 const testi = document.getElementById("client_testi"),
   img = document.getElementById("client_img"),
   client_name = document.getElementById("client_name"),
